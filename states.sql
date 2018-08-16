3056e554008a5281d0875fd8aea83e21d694639f
arashazizi1368 <arash.azizi1368@gmail.com>
2018-08-16 09:17:35 +0430

    Cities of Iran

---
+++ states.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 15, 2018 at 07:57 AM
+-- Server version: 5.6.40
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hipysir_salammoshaver`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `states`
+  ADD PRIMARY KEY (`id`);
+
+  ALTER TABLE `states`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+--
+-- Dumping data for table `states`
+--
+
+INSERT INTO `states` (`id`, `name`) VALUES
+(1, 'آذربايجان شرقي'),
+(2, 'آذربايجان غربي'),
+(3, 'اردبيل'),
+(4, 'اصفهان'),
+(5, 'البرز'),
+(6, 'ايلام'),
+(7, 'بوشهر'),
+(8, 'تهران'),
+(9, 'چهارمحال بختياري'),
+(10, 'خراسان جنوبي'),
+(11, 'خراسان رضوي'),
+(12, 'خراسان شمالي'),
+(13, 'خوزستان'),
+(14, 'زنجان'),
+(15, 'سمنان'),
+(16, 'سيستان و بلوچستان'),
+(17, 'فارس'),
+(18, 'قزوين'),
+(19, 'قم'),
+(20, 'كردستان'),
+(21, 'كرمان'),
+(22, 'كرمانشاه'),
+(23, 'كهكيلويه و بويراحمد'),
+(24, 'گلستان'),
+(25, 'گيلان'),
+(26, 'لرستان'),
+(27, 'مازندران'),
+(28, 'مركزي'),
+(29, 'هرمزگان'),
+(30, 'همدان'),
+(31, 'يزد');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
